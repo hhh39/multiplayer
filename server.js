f350639efebc690f10ca269a7153af4f7488dfc3
hhh39 <159802997+hhh39@users.noreply.github.com>
2026-05-09 14:31:50 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+var PORT = process.env.PORT || 3000;
+var express = require("express");
+var http = require("http");
+var { Server } = require("socket.io");
+var app = express();
+var server = http.createServer(app);
+var io = new Server(server);
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown", "grey", "black"]
+app.use(express.static("game"));
+var players = {};
+io.on("connection", socket => {
+    var color = colors.splice(Math.floor(Math.random() * availableColors.length), 1)[0];
+    console.log("Joined: " + socket.id);
+    console.log(players[socket.id]);
+    players[socket.id] = {
+        x: 100,
+        y: 100,
+	color: color
+    };
+    socket.on("move", data => {
+	if(!players[socket.id])return;
+        players[socket.id].x += data.dx;
+        players[socket.id].y += data.dy;
+    });
+    socket.on("disconnect", () => {
+	colors.push();(players[socket.id].color);
+        delete players[socket.id];
+	console.log("Left: " + socket.id);
+    });
+});
+setInterval(() => {
+    io.emit("state", players);
+}, 1000 / 30);
+server.listen(PORT, () => {
+    console.log("Server on port: " + PORT);
+});
